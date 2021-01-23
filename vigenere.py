e034b374b8c4861e0bdb3a6c9d7cf6a4fa1973c9
Ismail <ismailmajed5@gmail.com>
2021-01-23 16:30:41 -0700

    decode vigenere

---
+++ vigenere.py
@@ -0,0 +1,27 @@
+<<<<<<< HEAD
+=======
+#vigenere.py
+
+# Decode a Vigenère cipher on the message
+def decode_vigenere(message, secret):
+    newmsg = ""
+    for i in range(len(message)):
+        if(message[i].isupper()):
+            number = 65 + (ord(message[i]) - ord(secret[i%len(secret)])) % 26
+            newmsg += chr(number)
+        elif(message[i].islower()):
+            number = 97 + (ord(message[i]) - ord(secret[i%len(secret)])) % 26
+            newmsg += chr(number) 
+        else:
+            newmsg += message[i]
+    return newmsg
+
+# Performs the Vigenère cipher operation
+def encode_vigenere(message, secret):
+    newmsg = ""
+    for i in range(len(message)):
+        if(message[i].isupper()):
+            number = 65 + (ord(message[i]) + ord(secret[i%len(secret)]) - 2*65) % 26
+            newmsg += chr(number)
+    return newmsg
+>>>>>>> 029927e... Decode Vigenere and Add Art
